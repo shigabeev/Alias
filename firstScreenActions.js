b2adc791ea80e5fce80e1c67c8b2718f61d523b3
NickolayStorm2 <chesterlanduk@gmail.com>
2017-12-04 04:04:59 +0300

    Added addCommand functionals

--- firstScreenActions.js
+++ firstScreenActions.js
@@ -13,18 +13,30 @@ function loadCommands(){
 
     cell1.innerHTML = item;
     cell2.classList.add("delBut");
-    cell2.innerHTML = "<button onclick=\"delButton(this)\">—</button>"
+    cell2.innerHTML = "<button onclick=\"delCommand(this)\">—</button>"
 
   })
 }
 
 function addCommand(){
+  var newCommand = document.getElementById("commandInput").value
+  alert(newCommand)
+  COMMANDS.push(newCommand)
+
+  var table = document.getElementById("commands");
+  row = table.insertRow(0);
+  var cell1 = row.insertCell(0)
+  var cell2 = row.insertCell(1)
+
+  cell1.innerHTML = newCommand;
+  cell2.classList.add("delBut");
+  cell2.innerHTML = "<button onclick=\"delCommand(this)\">—</button>"
 
 }
 
-function delButton(btn){
+function delCommand(btn){
   var row = btn.parentNode.parentNode;
-  var removedItem = row.getElementsByTagName("td")[0].innerHTML
+  var removedItem = row.getElementsByTagName("td")[0].innerHTML;
   var i = COMMANDS.indexOf(removedItem);
   COMMANDS.splice(i, 1)
   row.parentNode.removeChild(row);
