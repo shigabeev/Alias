e809df6d511546cbd315f3b73921305179c4d5ec
Ilya Shigabeev <frappuccino_o@Kraken.local>
2017-12-05 00:35:33 +0300

    new structure

--- JS/game.js
+++ JS/game.js
@@ -44,3 +44,35 @@ function checkSecond(sec) {
     if (sec < 0) { sec = "59" };
     return sec;
 }
+
+function loadGame(){
+    var store = new Store();
+  
+    var span = document.getElementById("teamName")
+    command = store.getCurrentTeam();
+    span.innerHTML = command;
+  
+    var timer = document.getElementById("timer");
+    settings = store.getSettings();
+    timer.innerHTML = settings.timer;
+  
+    var word = document.getElementById("word");
+    word.innerHTML = store.getCurrentWord();
+  }
+  
+  function nextGame(){
+    var store = new Store();
+    store.nextTeam();
+    command = store.getCurrentTeam();
+    var span = document.getElementById("teamName")
+    span.innerHTML = command;
+  }
+  
+  function rightWord(){
+  
+  }
+  
+  function wrongWord(){
+  
+  }
+  
