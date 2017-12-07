093fdd0e51a9e40c3d5bccd48219bd8162b2585b
Ilya Shigabeev <frappuccino_o@Kraken.local>
2017-12-07 12:15:07 +0300

    added page

---
+++ JS/settings.js
@@ -0,0 +1,15 @@
+var rad = document.duration;
+var prev = null;
+for(var i = 0; i < rad.length; i++) {
+    rad[i].onclick = function() {
+        (prev)? alert(prev.value):null;
+        if(this !== prev) {
+            prev = this;
+        }
+        console.log(this.value)
+    };
+}
+
+function changeDuration (time){
+    alert("Gotcha !")
+}
