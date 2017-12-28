1fd9ff30fbbed11070aefd05de7f9f71d7302f96
NickolayStorm2 <chesterlanduk@gmail.com>
2017-12-28 13:25:16 +0300

    Fix isygdvfdb

--- js/results.js
+++ js/results.js
@@ -2,11 +2,13 @@ function init(){
     var winner = document.getElementById("winner")
     var store = new Store();
 
+    var results = store.getWordResult();
+
     var best;
     var best_sum = 0;
 
     Object.keys(results).forEach(function(team, _, _){
-        var sum;
+        var sum = 0;
         Object.keys(results[team]).forEach(function (word, _, _){
             sum = sum + results[team][word];
           })
@@ -16,5 +18,5 @@ function init(){
             best = team
           }
     });
-    winner.innerHTML = best;  
+    winner.innerHTML = best;
 }
